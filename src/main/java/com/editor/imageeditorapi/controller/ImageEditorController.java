d5000ae28d7bf9f413544f2494910e8eb0316e14
Christopher Nunez <elgary1129@gmail.com>
2025-01-22 20:10:28 -0300

    controlador para devolver la imagen editada y configuracion de cors creados

--- src/main/java/com/editor/imageeditorapi/controller/ImageEditorController.java
+++ src/main/java/com/editor/imageeditorapi/controller/ImageEditorController.java
@@ -1,4 +1,40 @@
 package com.editor.imageeditorapi.controller;
 
+import com.editor.imageeditorapi.service.ImageEditorService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
 public class ImageEditorController {
+
+    private ImageEditorService imageEditorService;
+
+    public ImageEditorController(ImageEditorService imageEditorService) {
+        this.imageEditorService = imageEditorService;
+    }
+
+    @PostMapping("/upload-image")
+    public ResponseEntity<byte[]> uploadImage(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("text") String text,
+            @RequestParam("size") int size) {
+        try {
+            byte[] imagen = imageEditorService.imageEditor(file, text, size);
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=output.png")
+                    .contentType(MediaType.IMAGE_PNG)
+                    .body(imagen);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }

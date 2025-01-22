8b4c7e45a2d2cc89c75e9a528213286d0852a98e
Christopher Nunez <elgary1129@gmail.com>
2025-01-22 19:57:32 -0300

    first commit

---
+++ src/main/java/com/editor/imageeditorapi/ImageEditorApiApplication.java
@@ -0,0 +1,13 @@
+package com.editor.imageeditorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageEditorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageEditorApiApplication.class, args);
+    }
+
+}

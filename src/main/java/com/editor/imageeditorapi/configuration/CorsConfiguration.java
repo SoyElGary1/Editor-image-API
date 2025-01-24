7c9349afa90abd7737eb4c1453b869772bcf4ef0
Christopher Nunez <elgary1129@gmail.com>
2025-01-23 22:36:14 -0300

    Dockerfile y variables de entorno agregados

--- src/main/java/com/editor/imageeditorapi/configuration/CorsConfiguration.java
+++ src/main/java/com/editor/imageeditorapi/configuration/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package com.editor.imageeditorapi.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
+    @Value("${app.allowed.origins}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("POST");
     }
 }
--- src/main/java/com/editor/imageeditorapi/service/ImageEditorService.java
+++ src/main/java/com/editor/imageeditorapi/service/ImageEditorService.java
@@ -1,8 +1,5 @@
 package com.editor.imageeditorapi.service;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
